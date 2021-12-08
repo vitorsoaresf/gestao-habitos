8bac3a333834e62fe8aa2775809fa3e5902486b8
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2021-12-08 18:33:50 -0300

    feat: creating cardGroup

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
 import { Container } from "./styles";
+import CardGroups from "../../components/CardGroups";
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      Home
+      <CardGroups title={"Activities"}></CardGroups>
+    </Container>
+  );
 };
 
 export default Home;
