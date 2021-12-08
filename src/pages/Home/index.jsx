472c5d885377f68550a6fd887f7d45a34a640c8d
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2021-12-08 18:41:08 -0300

    fix: fix testing component in home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,7 @@
 import { Container } from "./styles";
-import CardGroups from "../../components/CardGroups";
 
 const Home = () => {
-  return (
-    <Container>
-      Home
-      <CardGroups title={"Activities"}></CardGroups>
-    </Container>
-  );
+  return <Container>Home</Container>;
 };
 
 export default Home;
