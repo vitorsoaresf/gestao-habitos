df6d1e5eb55d834f5a5fb17f9bcada6a543aeaeb
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2021-12-08 14:29:29 -0300

    fix: importing GlobalStyle in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import GlobalStyle from "./style/global";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <GlobalStyle />
     </div>
   );
 }
