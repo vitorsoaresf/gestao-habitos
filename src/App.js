af5e68081f2efb9567b5d4509ea381d3d7c2dfce
Emerson Pereira <ebergp@gmail.com>
2021-12-09 08:54:42 -0300

    feat: implementing Input

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import GlobalStyle from "./style/global";
-
 import CardGeneric from "./components/CardGeneric";
+
+import Input from "./components/Input";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <GlobalStyle />
-        <CardGeneric title="Teste" />
+        <CardGeneric />
+        <Input />
       </header>
     </div>
   );
