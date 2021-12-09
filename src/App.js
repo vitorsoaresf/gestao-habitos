2e897c3b79900de4e2e88e75cc9f3a3c6c357f2a
Emerson Pereira <ebergp@gmail.com>
2021-12-09 08:58:29 -0300

    merge: merges Input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import "./App.css";
-import Footer from "./components/Footer";
-import Modal from "./components/Modal";
 import Routes from "./routes/Routes";
 import GlobalStyle from "./style/global";
 
