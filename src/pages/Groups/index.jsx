9a0fbd87d0d8205add108da61c4f57bd53f660ba
Leonardo <leonardomlouzas@gmail.com>
2021-12-10 11:45:44 -0300

    fix: refactors isAuthenticated verification in Groups page

--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -1,16 +1,13 @@
-import { useContext } from "react";
-import { AuthenticatedContext } from "../../providers/authenticated";
 import { Redirect } from "react-router-dom";
 import { Container } from "./styles";
 import Header from "../../components/Header";
 import CardGeneric from "../../components/CardGeneric";
 
 const Groups = () => {
-  const { authenticated } = useContext(AuthenticatedContext);
-
-  if (!authenticated) {
+  if (!JSON.parse(localStorage.getItem("@Anima/authenticated"))) {
     return <Redirect to="/" />;
   }
+
   return (
     <Container>
       <Header />
