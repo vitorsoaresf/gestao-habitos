3db1a705f481eae3ca61117033d958f57c1ba7a1
Emerson Pereira <ebergp@gmail.com>
2021-12-15 23:29:53 -0300

    feat: includes vertical scroll bar

--- src/components/CardGeneric/styles.js
+++ src/components/CardGeneric/styles.js
@@ -61,10 +61,24 @@ export const ListBox = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+
   /* background: red; */
 
   ul {
     overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+      width: 8px;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: transparent;
+      border-radius: 100px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: var(--white);
+      border-radius: 100px;
+    }
+
     > li {
       background: white;
       width: 250px;
